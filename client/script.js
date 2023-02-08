3e85edbda7f37d609052b4e44ec3d73b8ce8834a
pavpench <p.penchev90@gmail.com>
2023-02-09 00:22:03 +0100

    initiating deployment

--- client/script.js
+++ client/script.js
@@ -72,7 +72,7 @@ const handleSubmit = async (e) => {
   loader(messageDiv);
 
   //fetch data from server
-  const response = await fetch("http://localhost:5000", {
+  const response = await fetch("https://companion-jibm.onrender.com", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
